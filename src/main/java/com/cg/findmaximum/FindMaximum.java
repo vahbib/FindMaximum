6a93a301e9b7793867c796a080f64aeeac1fcb5f
vahbib <singhbibhav084@gmail.com>
2020-10-15 21:13:13 +0530

    Initialization

---
+++ src/main/java/com/cg/findmaximum/FindMaximum.java
@@ -0,0 +1,8 @@
+package com.cg.findmaximum;
+
+public class FindMaximum {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Find Maximum Problem of GENERIC");
+    }
+
+}
---
+++ src/test/java/com/cg/findmaxtest/FindMaximumTest.java
@@ -0,0 +1,5 @@
+package com.cg.findmaxtest;
+
+public class FindMaximumTest {
+
+}
