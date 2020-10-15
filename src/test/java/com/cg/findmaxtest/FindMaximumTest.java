497bc38cf5046fcb42a08b3ad32d2a603bdf4f6a
vahbib <singhbibhav084@gmail.com>
2020-10-15 22:29:35 +0530

    Added - UserCase2 - given three floats find the max

--- src/test/java/com/cg/findmaxtest/FindMaximumTest.java
+++ src/test/java/com/cg/findmaxtest/FindMaximumTest.java
@@ -1,5 +1,18 @@
 package com.cg.findmaxtest;
 
-public class FindMaximumTest {
+import com.cg.findmaximum.FindMaximum;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class FindMaximumTest {
+    @Test
+    public void Test() {
+        FindMaximum fM = new FindMaximum();
+        Assert.assertEquals(56, fM.maxInt(24,56,12));
+    }
+    @Test
+    public void TestF() {
+        FindMaximum fM = new FindMaximum();
+        Assert.assertEquals (56.0, fM.maxFloat(24.0f,56.0f,12.0f), 0.0f);
+    }
 }
