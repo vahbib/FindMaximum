26674d284f776c31762c152a4c9c85715b347494
vahbib <singhbibhav084@gmail.com>
2020-10-16 00:24:56 +0530

    Added - UC5 - Extend the max method to also print the max to std out using generic method

--- src/test/java/com/cg/findmaxtest/FindMaximumTest.java
+++ src/test/java/com/cg/findmaxtest/FindMaximumTest.java
@@ -1,5 +1,30 @@
 package com.cg.findmaxtest;
 
-public class FindMaximumTest {
+import com.cg.findmaximum.FindMaximum;
+import org.junit.Assert;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FindMaximumTest {
+    @Test
+    public void maximumTest(){
+        Integer[] arr={1,54,62,88,100};
+        FindMaximum fM=new FindMaximum(arr);
+        Display(arr);
+        Assert.assertEquals(100,fM.maxOf());
+    }
+    public static <E extends Comparable<E>> E Display(E[] element) {
+        List<E> list = new ArrayList<E>();
+        for (E e : element) {
+            // Add each element into the list
+            list.add(e);
+        }
+        Collections.sort(list);
+        System.out.println("Maximum is: "+ list.get(list.size()-1));
+        return list.get(list.size()-1);
+    }
 }
