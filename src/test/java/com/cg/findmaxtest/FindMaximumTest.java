710dceac853320610f63923cb5dadd21ceaed492
Bibhav Singh <71620937+vahbib@users.noreply.github.com>
2020-10-15 22:27:33 +0530

    Update FindMaximumTest.java

--- src/test/java/com/cg/findmaxtest/FindMaximumTest.java
+++ src/test/java/com/cg/findmaxtest/FindMaximumTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 
 public class FindMaximumTest {
     public void Test() {
+        @Test
         FindMaximum fM = new FindMaximum();
         Assert.assertEquals(56, fM.maxInt(24,56,12));
     }
