bcdad56b8803273881f8da8c6922acbe5e3ea120
vahbib <singhbibhav084@gmail.com>
2020-10-15 23:14:04 +0530

    Added - RefactorCase - Refactor All 3 in Generic Type

--- src/main/java/com/cg/findmaximum/FindMaximum.java
+++ src/main/java/com/cg/findmaximum/FindMaximum.java
@@ -1,8 +1,24 @@
 package com.cg.findmaximum;
 
-public class FindMaximum {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Find Maximum Problem of GENERIC");
+public class FindMaximum<E extends Comparable> {
+    E a,b,c;
+    public FindMaximum(E a, E b, E c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+    }
+    public E maxOf() {
+
+        if(a.compareTo(b)>0 && a.compareTo(c)>0){
+            return a;
+        }
+        else if(b.compareTo(a)>0 && b.compareTo(c)>0){
+            return b;
+        }
+        else {
+            return c;
+        }
     }
 
 }
--- src/test/java/com/cg/findmaxtest/FindMaximumTest.java
+++ src/test/java/com/cg/findmaxtest/FindMaximumTest.java
@@ -1,5 +1,14 @@
 package com.cg.findmaxtest;
 
+import com.cg.findmaximum.FindMaximum;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FindMaximumTest {
+    @Test
+    public void Test() {
+        FindMaximum fM = new FindMaximum("A", "b", "Z");
+        Assert.assertEquals("b", fM.maxOf());
+    }
 
 }
