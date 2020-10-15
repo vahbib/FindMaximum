bf297fc5f4894c67d6cad9fcc87afc456a8f175b
vahbib <singhbibhav084@gmail.com>
2020-10-16 00:03:55 +0530

    Added - UserCAse4 - Extend the max method to take more than 3 parameters

--- src/main/java/com/cg/findmaximum/FindMaximum.java
+++ src/main/java/com/cg/findmaximum/FindMaximum.java
@@ -1,8 +1,31 @@
 package com.cg.findmaximum;
 
-public class FindMaximum {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Find Maximum Problem of GENERIC");
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FindMaximum<E extends Comparable> {
+//    public static void main(String[] args) {
+//        System.out.println("Welcome to Find Maximum Problem of GENERIC");
+//    }
+    E array[];
+    public FindMaximum(E[] array) {
+        this.array = array;
+
+    }
+    public E maxOf() {
+        List<E> list = new ArrayList<E>();
+        // Array to List Conversion
+        for (E e : array) {
+            // Add each element into the list
+            list.add(e);
+        }
+        System.out.println("Maximum of these is: "+ Collections.max(list, null));
+        return Collections.max(list, null);
+
+//        Collections.sort(list);
+//        System.out.println("Maximum is: "+ list.get(list.size()-1));
+
     }
 
 }
--- src/test/java/com/cg/findmaxtest/FindMaximumTest.java
+++ src/test/java/com/cg/findmaxtest/FindMaximumTest.java
@@ -1,5 +1,14 @@
 package com.cg.findmaxtest;
 
-public class FindMaximumTest {
+import com.cg.findmaximum.FindMaximum;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class FindMaximumTest {
+    @Test
+    public void maximumTest(){
+        Integer[] arr={1,54,62,88,100};
+        FindMaximum fM=new FindMaximum(arr);
+        Assert.assertEquals(100,fM.maxOf());
+    }
 }
